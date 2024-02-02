7a0fbe3966a29fb38e95307e46a6dc5cd1ed2404
greenvirs <d.perfilov@magnus-tech.ru>
2024-02-02 17:27:39 +0400

    Добавлен слайдер

---
+++ components/VSlider.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  alt: string
+  images: string[]
+}>()
+</script>
+
+<template>
+  <div :class="$style['container']">
+    <img v-for="image in images" :class="$style['img']" :src="image" :alt="alt" />
+  </div>
+</template>
+<style module>
+.container {
+  width: 100%;
+  display: flex;
+  overflow-y: hidden;
+  overflow-x: auto;
+}
+.img {
+  display: block;
+  width: 100%;
+  height: auto;
+  flex: 1 1 100%
+}
+</style>
